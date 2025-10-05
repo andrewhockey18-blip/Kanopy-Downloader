771e812e2cbe805cd0258775c5ae0e3ab8780a1c
andrewhockey18-blip <andrewhockey18@gmail.com>
2025-10-05 19:33:50 -0400

    Update Header.py

---
+++ Header.py
@@ -0,0 +1,39 @@
+cookies = {
+    '__cfruid': '41a93d023c7c27b6c92fe94fa559e4e3ea9bf098-1759706118',
+    '_cfuvid': 'xmQQgS0OWi3fFK9M0c03Umd2m48FYswYBGjHUFq9uxU-1759706118015-0.0.1.1-604800000',
+    'cf_clearance': 'VKMQODo3DWwYVNJfAR420_iZeE41H9kGxdxdrNXVt_4-1759706118-1.2.1.1-H7cx22W8ujDnX7BQEfndDil9MUBlRIPWyU3xh.L81I8LkvrmtvjDmcgxEpUn0wJtD1mzXVrgGokCaR.DqMzNa7QX.UfljPM3qNsgK._lTjFMJNzoYvGOAgrFRwopw9BkSeMNNe1NQ2n60UXmrwQL4yahdXHGYgprixdtGSxn02BCBL5ajWmBJOidjhp3vRQcX3wS3wvwOAz1iVliietbpsnkDbtwMqNRPCqLdyBbWIw',
+    'kapi_token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVpZCI6IjAiLCJpZGVudGl0eV9pZCI6IjAiLCJ2aXNpdG9yX2lkIjoiMTc1OTcwNjExODYxMjAwMjM3NSIsInNlc3Npb25faWQiOiIxNzU5NzA2MTE4NjEyMDU0ODgzIiwiY29ubmVjdGlvbl9pZCI6IjE3NTk3MDYxMTg2MTIwNTQ4ODMiLCJrdWlfdXNlciI6MSwicm9sZXMiOltdfSwiaWF0IjoxNzU5NzA2MTE4LCJleHAiOjIwNzUwNjYxMTgsImlzcyI6ImthcGkifQ.CNhwbvdnD3bGH1cy1tKJ5CQD6-KfhdtBBzKul5kJocE',
+    'token_set': '%222025-10-05T23%3A15%3A18.610Z%22',
+    'visitor_id': '1759706118612002375',
+    'webshop': '%7B%22id%22%3A2%2C%22code%22%3A%22US%22%2C%22name%22%3A%22United%20States%22%7D',
+    '_gcl_au': '1.1.3194399.1759706121',
+    '103_1759706118612002375_visitor_identity': '517816099',
+    '_ga': 'GA1.1.40849887.1759706125',
+    '_ga_GGQJZHYDLG': 'GS2.1.s1759706125$o1$g1$t1759706836$j60$l0$h0',
+    'muxData': '=undefined&mux_viewer_id=7803e7e1-c05a-4c45-b3dd-9c17b6a8d29e&msn=0.21399797633666484&sid=f213a3ef-d338-4b97-b0ce-ed11b6a8d2b3&sst=1759706141363&sex=1759708336877',
+}
+
+headers = {
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'en-US,en;q=0.9',
+    'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVpZCI6IjAiLCJpZGVudGl0eV9pZCI6IjAiLCJ2aXNpdG9yX2lkIjoiMTc1OTcwNjExODYxMjAwMjM3NSIsInNlc3Npb25faWQiOiIxNzU5NzA2MTE4NjEyMDU0ODgzIiwiY29ubmVjdGlvbl9pZCI6IjE3NTk3MDYxMTg2MTIwNTQ4ODMiLCJrdWlfdXNlciI6MSwicm9sZXMiOltdfSwiaWF0IjoxNzU5NzA2MTE4LCJleHAiOjIwNzUwNjYxMTgsImlzcyI6ImthcGkifQ.CNhwbvdnD3bGH1cy1tKJ5CQD6-KfhdtBBzKul5kJocE',
+    'content-type': 'application/json',
+    'origin': 'https://www.kanopy.com',
+    'priority': 'u=1, i',
+    'referer': 'https://www.kanopy.com/en/unh/watch/customplaylists/12042175/43818626',
+    'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
+    'x-version': 'web/prod/5.13.0/2025-09-23-13-02-50',
+    # 'cookie': '__cfruid=41a93d023c7c27b6c92fe94fa559e4e3ea9bf098-1759706118; _cfuvid=xmQQgS0OWi3fFK9M0c03Umd2m48FYswYBGjHUFq9uxU-1759706118015-0.0.1.1-604800000; cf_clearance=VKMQODo3DWwYVNJfAR420_iZeE41H9kGxdxdrNXVt_4-1759706118-1.2.1.1-H7cx22W8ujDnX7BQEfndDil9MUBlRIPWyU3xh.L81I8LkvrmtvjDmcgxEpUn0wJtD1mzXVrgGokCaR.DqMzNa7QX.UfljPM3qNsgK._lTjFMJNzoYvGOAgrFRwopw9BkSeMNNe1NQ2n60UXmrwQL4yahdXHGYgprixdtGSxn02BCBL5ajWmBJOidjhp3vRQcX3wS3wvwOAz1iVliietbpsnkDbtwMqNRPCqLdyBbWIw; kapi_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVpZCI6IjAiLCJpZGVudGl0eV9pZCI6IjAiLCJ2aXNpdG9yX2lkIjoiMTc1OTcwNjExODYxMjAwMjM3NSIsInNlc3Npb25faWQiOiIxNzU5NzA2MTE4NjEyMDU0ODgzIiwiY29ubmVjdGlvbl9pZCI6IjE3NTk3MDYxMTg2MTIwNTQ4ODMiLCJrdWlfdXNlciI6MSwicm9sZXMiOltdfSwiaWF0IjoxNzU5NzA2MTE4LCJleHAiOjIwNzUwNjYxMTgsImlzcyI6ImthcGkifQ.CNhwbvdnD3bGH1cy1tKJ5CQD6-KfhdtBBzKul5kJocE; token_set=%222025-10-05T23%3A15%3A18.610Z%22; visitor_id=1759706118612002375; webshop=%7B%22id%22%3A2%2C%22code%22%3A%22US%22%2C%22name%22%3A%22United%20States%22%7D; _gcl_au=1.1.3194399.1759706121; 103_1759706118612002375_visitor_identity=517816099; _ga=GA1.1.40849887.1759706125; _ga_GGQJZHYDLG=GS2.1.s1759706125$o1$g1$t1759706836$j60$l0$h0; muxData==undefined&mux_viewer_id=7803e7e1-c05a-4c45-b3dd-9c17b6a8d29e&msn=0.21399797633666484&sid=f213a3ef-d338-4b97-b0ce-ed11b6a8d2b3&sst=1759706141363&sex=1759708336877',
+}
+
+json_data = {
+    'videoId': 143903,
+    'visitorId': '1759706118612002375',
+    'domainId': 103,
+}
